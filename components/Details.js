4ae2a8b251383ac0d8df86279da4a49b14878383
MVAodhan <hamilton.aodhan@gmail.com>
2022-05-27 14:09:06 +1200

    change font size for links

--- components/Details.js
+++ components/Details.js
@@ -7,14 +7,10 @@ const Details = ({ name, title, link }) => {
       display="flex"
       flexDirection="column"
       pl={{ base: '10px', sm: '25px', lg: '15px' }}
+      fontSize={{ base: '12px', sm: '14px', md: '16px', lg: '14px' }}
     >
       <Text fontSize="20px"> {name} </Text>
-      <Text
-        fontSize={{ base: '12px', sm: '14px', md: '16px', lg: '14px' }}
-        as="i"
-      >
-        {title}
-      </Text>
+      <Text as="i">{title}</Text>
       <Text color="#03fcfc" as="i">
         <Link href={link}>{link}</Link>
       </Text>
